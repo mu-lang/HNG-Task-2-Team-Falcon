8d2d2df1f78f0c41876de2445391f36fadf81bf1
mu-lang <66227341+mu-lang@users.noreply.github.com>
2020-06-08 00:30:23 +0100

    Update HNG-03075.py

--- HNG-03075.py
+++ HNG-03075.py
@@ -1,1 +1,7 @@
-print("Hello World, this is Muhammad Ibrahim with HNGi7 ID HNG-03075 using python for stage 2 task. muhammadib113@gmail.com")
+
+name  = "Muhammad Ibrahim"
+hngId = "HNG-03075"
+language = "python"
+email = "muhammadib113@gmail.com"
+print("Hello World, this is %s with HNGi7 ID %s using %s for stage 2 task. %s"%(name,hngId,language,email))
+
